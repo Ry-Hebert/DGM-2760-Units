f1f3261637a3043ee1d069036a194abf70224a23
Ryan <ryan@thecpafirm.com>
2020-02-29 13:41:42 -0700

    Updated

--- DGM-2760-04/assets/js/main.js
+++ DGM-2760-04/assets/js/main.js
@@ -1,92 +1,97 @@
-let guessCount = 0
-let findVar = 15
 randomGen = (x) =>{return window.crypto.getRandomValues(new Uint32Array(1))[0] % x}
 // Because we're trying to get a random number between 1 and 15 we are going to pass 15 (Our number of numbers we're looking for) add then add +1 to the results of randomGen(15)(Which should provided us with numbers between 0-14) so that our value of randomNumber will be somewhere between 1-15.
-const randomNumber = randomGen(findVar) + 1
+//const randomNumber = randomGen(findVar) + 1
 // Generates a more cryptographically secure random number than simply (Math.random). The % operator returns the remainder of the random number divided by 15.
-
-
-checkGuess = (x, y) =>
+minMaxRandomNumGen = (min, max) =>
 {
-    let check = x == y
-    console.log(check)
-    if(check == true){return check}
-    else
-    {
-        if(x > y){return 'Your Guess was Too High'}
-        else{return 'Your Guess was Too Low'}
-    }
+    let x = (max +1) - min;
+    let ranNum = randomGen(x) + min
+    return ranNum
 }
 
 contentLoad = () =>
 {
     let pos1 = document.querySelector('#pageTitle')
     let pos2 = document.querySelector('#pageSlogan')
+    let tellF = document.querySelector('#fortune')
 
-    pos1.textContent=`Guessing Game`
-    pos2.textContent=`This is a simple number based guessing game.`
-}
+    pos1.textContent=`The Fortune Teller`
+    pos2.textContent=`What mysteries await you!`
 
-submitGuess = () =>
-{
-    let feedbackR = null
-    let tryCountR = null
-    let playerAwardR = null
-    // Logged to Console for testing purposes.
-    console.log(`Random Number is: ${randomNumber}`)
-    let guess = document.querySelector('#numberGuess').value;
+    let monthNum = minMaxRandomNumGen(1, 12)
+    let dayNum = minMaxRandomNumGen(1, 30)
+    let fortuneNum = minMaxRandomNumGen(1, 5)
 
-    // Check to see if value is grabbed
-    console.log(`Player guess is: ${guess}`);
+    let month = null;
+    let fortune = null;
 
-    if(guess < 1 || guess > 15)
+    switch(monthNum)
     {
-        feedbackR = document.querySelector('#feedback')
-        feedbackR.textContent = 'Please chose a number between 1 and 15'
+        case 1:
+            month = 'January'
+            break 
+        case 2: 
+            month = 'February'
+            break 
+        case 3:
+            month = 'March'
+            break 
+        case 4: 
+            month = 'April'
+            break 
+        case 5:
+            month = 'May'
+            break 
+        case 6: 
+            month = 'June'
+            break 
+        case 7:
+            month = 'July'
+            break 
+        case 8: 
+            month = 'August'
+            break 
+        case 9:
+            month = 'September'
+            break 
+        case 10: 
+            month = 'October'
+            break 
+        case 11:
+            month = 'November'
+            break 
+        case 12: 
+            month = 'December'
+            break
+        default: 
+            month = 'Unknown Error'
+            console.log(`Month # = ${monthNum}`)
+            break
     }
-    else
+    
+    switch(fortuneNum)
     {
-        let check = checkGuess(guess, randomNumber)
-
-        if(check == true)
-        {
-            feedbackR = document.querySelector('#feedback')
-            feedbackR.textContent = 'You are Correct!'
-
-            guessCount++
-            tryCountR = document.querySelector('#tryCount')
-            tryCountR.textContent = guessCount
-            switch(guessCount)
-            {
-                case 1:
-                case 2:
-                case 3:
-                    playerAwardR = document.querySelector('#playerAward')
-                    playerAwardR.className = 'firstP'
-                    break
-                case 4:
-                case 5:
-                case 6:
-                    playerAwardR = document.querySelector('#playerAward')
-                    playerAwardR.className = 'secondP'
-                    break
-                default:
-                    playerAwardR = document.querySelector('#playerAward')
-                    playerAwardR.className = 'thirdP'
-                    break
-
-            }
-
-        }
-        else
-        {
-            feedbackR = document.querySelector('#feedback')
-            feedbackR.textContent = check
-            guessCount++
-            tryCountR = document.querySelector('#tryCount')
-            tryCountR.textContent = guessCount
-        }
-
+        case 1:
+            fortune = 'you will find a slip of paper with numbers and letters on it only to realize its a bitcoin wallet address. upon looking into it more you find it dosen\'t have anything in it.'
+            break
+        case 2:
+            fortune = 'your hobby will be discovered by someone very influential (and no, not your sister in law who is trying to be a \'social media influencer\'.)'
+            break
+        case 3:
+            fortune = 'something magical awaits you.'
+            break
+        case 4:
+            fortune = 'just bring a umbrella, trust me.'
+            break
+        case 5:
+            fortune = `your lucky numbers are: ${minMaxRandomNumGen(0, 99)}, ${minMaxRandomNumGen(0, 99)}, ${minMaxRandomNumGen(0, 99)}, ${minMaxRandomNumGen(0, 99)}, ${minMaxRandomNumGen(0, 99)}`
+            break
+        default:
+            fortune = 'Unknown Error'
+            console.log(`Fortune # = ${fortuneNum}`)
+            break
     }
+
+    tellF.textContent = `On ${month} ${dayNum} ${fortune}`
 
 }
