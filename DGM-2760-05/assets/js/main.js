07c216ed2906aba59b72990c48ce65166438603f
Ryan <ryan@thecpafirm.com>
2020-03-12 12:33:43 -0600

    updates and trial

--- DGM-2760-05/assets/js/main.js
+++ DGM-2760-05/assets/js/main.js
@@ -8,8 +8,8 @@ contentLoad = () =>
     let pos2 = document.querySelector('#pageSlogan')
     let tellF = document.querySelector('#fortune')
 
-    pos1.textContent=`The Fortune Teller`
-    pos2.textContent=`What mysteries await you!`
+    pos1.textContent=`The Arborist`
+    pos2.textContent=``
 
     displayTrees()
 }
@@ -20,3 +20,4 @@ displayTrees = () =>
     tArr.forEach(x => {aOfTrees += `${x} <br>`})
     output.innerHTML = `<p>${tArr}</p>`
 }
+
