ae70d12a4b3eb671cd03880b1aead78057feb1b7
Ryan <ryan@thecpafirm.com>
2020-04-21 16:23:15 -0600

    U9 working I think

--- DGM-2760-09/assets/js/main.js
+++ DGM-2760-09/assets/js/main.js
@@ -1,14 +1,20 @@
-menuDup () =>
+menuDup = () =>
 {
-    let toplist = document.querySelectorAll('')
+    let topList = document.querySelectorAll('ul#primaryNavigation li a')
 
     let newList = document.createElement('ul')
+    document.querySelector('#smallNavArea').appendChild(newList)
 
-    toplist.forEach(menuItem =>
+    topList.forEach(menuItem =>
         {
             let newLi = document.createElement('li')
             let newLink = document.createElement('a')
             newLink.setAttribute('href', menuItem.getAttribute('href'))
-
+            
+            newLink.textContent = menuItem.textContent
+            newList.appendChild(newLi)
+            newLi.appendChild(newLink)
         })
 }
+
+menuDup()
